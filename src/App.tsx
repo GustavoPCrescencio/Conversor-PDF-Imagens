44bd16cc5561a402b9d6372cfac8c9bbf6bac97e
Gustavo Paes <gustavo.paes1414@gmail.com>
2026-06-16 15:10:16 -0300

    Resolvendo bug de páginas

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import ImagensParaPdf from './pages/ImagensParaPdf'
 import PdfParaImagens from './pages/PdfParaImagens'
-import MisturarArquivos from './pages/MisturarArquivos'
 import './App.css'
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/imagens-para-pdf" element={<ImagensParaPdf />} />
         <Route path="/pdf-para-imagens" element={<PdfParaImagens />} />
-        <Route path="/misturar-arquivos" element={<MisturarArquivos/>} />
       </Routes>
     </BrowserRouter>
   )
